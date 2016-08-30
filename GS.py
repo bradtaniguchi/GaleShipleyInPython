940c287661757b39559208ba92e0634f5d526281
Brad <bradtaniguchi@gmail.com>
2016-08-30 15:53:09 -0700

    Pre fix changes

--- GS.py
+++ GS.py
@@ -35,13 +35,14 @@ def main(numberOfCouples, verbose):
     num = numberOfCouples
     men = []
     women = []
+    proposals = 0
     for i in range(0,num):
         men.append(Person("man" + str(i), True))
         women.append(Person("woman" + str(i), False))
     
     # after generating the list of men and women
     # randomly generate the preferences lists for each
-    if(verbose): print("=========Defining Preferences========")
+    if(verbose): print("========Defining Preferences======")
     for man in men:
         man.generateRandPref(women)
         if(verbose): print(man.name.ljust(10) + " [ ", end="")
@@ -53,22 +54,28 @@ def main(numberOfCouples, verbose):
         if(verbose): woman.getPref()
         if(verbose): print("]")
 
-    if(verbose): print("=======End Preferences======")
+    if(verbose): print("========End Preferences===========")
     # now the preferences are done, start propositions
     lonelyMen = list(men)  # men that haven't propsed
     while len(lonelyMen) > 0:  # while there is a man that hasn't proposed
+        proposals += 1
         readyMan = lonelyMen.pop(0)  # get man at top of "stack"
         # check preference list....
-        crush = readyMan.pref.pop()  # pop top woman in "stack"
+        crush = readyMan.pref.pop(0)  # pop top woman in "stack"
+
+        #print("debug: " + str(crush.pref.index(readyMan)) + " > " + str(crush.pref.index(crush.fiance)))
+        
         # and see if woman is engaged
         if (crush.fiance is None) :  # if woman isn't engaged
             # BECOME engaged (yay)
             crush.fiance = readyMan  # set the man as the fiance!s
-            readyMan.fiance = crush# set the woman as the fiance
+            readyMan.fiance = crush  # set the woman as the fiance
+            print("DEBUG: " + str(crush.pref.index(readyMan)))
         #TODO: is this object reference comparison
         elif (crush.pref.index(readyMan) > crush.pref.index(crush.fiance)): 
             # according to crush, the pos of our readyMan, is greater than 
             #NOTE: pop removes the LAST item! 
+            print("elif")
             Person(crush.fiance).fiance = None  # set the fiance of the fiance's 
             crush.fiance = readyMan # set the man as the fiance's
             readyMan.fiance = checkWoman # set the woman as the fiance
@@ -77,18 +84,20 @@ def main(numberOfCouples, verbose):
         else:  # here for clarity
             continue
             
-    # return the set of S of engaged pairs.
-    print("Couples engaged: " + str(len(men)))
     for man in men:
         print("Couple: " + man.name + " <---> " + man.fiance.name)
     for woman in women:
         print("Couple: " + woman.name + " <---> " + woman.fiance.name)
-
+    print("------------------------")
+    print(" n = " + str(numberOfCouples))
+    n2 = numberOfCouples * numberOfCouples
+    print(" n^2 = " + str(n2))
+    print("Proposals: " + str(proposals))
 
 if __name__ == '__main__':
     if len(sys.argv) == 1: 
         print("Not enough arguments given!\n" +
-              "GS.py [Options] NumberOfCouples\n" + 
+              "GS.py [Options] numberOfCouples\n" + 
               "  -v : Verbose - Provides all extra printouts\n")
         sys.exit(1)  # not enough arguments!
     else:
