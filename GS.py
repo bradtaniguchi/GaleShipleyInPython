548b0f1ecdf699b1f71d83fee602e4650452f151
Brad <bradtaniguchi@gmail.com>
2016-08-30 17:04:39 -0700

    Fixed issue with n proposals

--- GS.py
+++ GS.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 import sys  # to get command line arguments
 import random  # to generate random preferences
 
@@ -9,20 +9,14 @@ class Person():
     :param name: string - name of person
     :param man: boolean - if this person is a male
     '''
-    def __init__(self, name, male, prefList=[]):
-        if len(prefList) != 0:
-            _generateRandPref(prefList)
-        else:
-            self.pref = []
+    def __init__(self, name, male):
+        self.pref = []
         self.name = name  # string
         self.male = male  # boolean, not really used
         self.fiance = None
 
-    def addPref(self, list):
-        self.pref = list
-
     def generateRandPref(self, listOfOtherGender):
-        self.pref = listOfOtherGender
+        self.pref = list(listOfOtherGender)
         random.shuffle(self.pref)  # does inline, returns nothing!
     
     def getPref(self, spacing=8):
@@ -45,11 +39,13 @@ def main(numberOfCouples, verbose):
     if(verbose): print("========Defining Preferences======")
     for man in men:
         man.generateRandPref(women)
+
         if(verbose): print(man.name.ljust(10) + " [ ", end="")
         if(verbose): man.getPref()
         if(verbose): print("]")
     for woman in women:
         woman.generateRandPref(men)
+
         if(verbose): print(woman.name.ljust(10) + " [ ", end="")
         if(verbose): woman.getPref()
         if(verbose): print("]")
@@ -57,38 +53,39 @@ def main(numberOfCouples, verbose):
     if(verbose): print("========End Preferences===========")
     # now the preferences are done, start propositions
     lonelyMen = list(men)  # men that haven't propsed
+    if(verbose): print("========Start Engagements=========")
     while len(lonelyMen) > 0:  # while there is a man that hasn't proposed
         proposals += 1
         readyMan = lonelyMen.pop(0)  # get man at top of "stack"
-        # check preference list....
-        crush = readyMan.pref.pop(0)  # pop top woman in "stack"
+        if(verbose): print(readyMan.name + " is looking for a fiance...") 
 
-        #print("debug: " + str(crush.pref.index(readyMan)) + " > " + str(crush.pref.index(crush.fiance)))
-        
+        crush = readyMan.pref.pop(0)  # pop top woman in "stack"
+        if(verbose): print(readyMan.name + " is proposing to: " + crush.name)
         # and see if woman is engaged
         if (crush.fiance is None) :  # if woman isn't engaged
-            # BECOME engaged (yay)
             crush.fiance = readyMan  # set the man as the fiance!s
             readyMan.fiance = crush  # set the woman as the fiance
-            print("DEBUG: " + str(crush.pref.index(readyMan)))
-        #TODO: is this object reference comparison
-        elif (crush.pref.index(readyMan) > crush.pref.index(crush.fiance)): 
-            # according to crush, the pos of our readyMan, is greater than 
-            #NOTE: pop removes the LAST item! 
-            print("elif")
-            Person(crush.fiance).fiance = None  # set the fiance of the fiance's 
-            crush.fiance = readyMan # set the man as the fiance's
-            readyMan.fiance = checkWoman # set the woman as the fiance
-
-        # crush prefers her current fiance to readyMan
-        else:  # here for clarity
-            continue
-            
+        #elif (crush.pref.index(readyMan) > crush.pref.index(crush.fiance)): 
+        else: # w is currently engaged with someone else
+            otherMan = crush.fiance
+            if (crush.pref.index(readyMan) > crush.pref.index(otherMan)):
+                if(verbose): 
+                    print(readyMan.name +" ("+ str(crush.pref.index(readyMan)) +") " + 
+                        " got dejected by " + crush.name)
+                lonelyMen.append(readyMan)  # no longer ready, push to top of stack
+            else:  # our readyMan is liked BETTER
+                if(verbose): 
+                    print(readyman.name + " is liked better than " + otherMan.name)
+                otherMan.fiance = None
+                lonelyMen.append(otherMan)  # no longer ready, push to top of stack
+                crush.fiance = readyMan
+                readyMan.fiance = crush
+    if(verbose): print("==========End Engagements=========")
     for man in men:
         print("Couple: " + man.name + " <---> " + man.fiance.name)
-    for woman in women:
-        print("Couple: " + woman.name + " <---> " + woman.fiance.name)
-    print("------------------------")
+
+    print("\n------------------------")
+    print("Algorithm Stats:")
     print(" n = " + str(numberOfCouples))
     n2 = numberOfCouples * numberOfCouples
     print(" n^2 = " + str(n2))
