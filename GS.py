cfce768cb6a77520d12a65b6a067d27d7a2f8964
Brad <bradtaniguchi@gmail.com>
2016-09-07 16:11:24 -0700

    Added more algorithm analysis

--- GS.py
+++ GS.py
@@ -2,6 +2,8 @@
 import sys  # to get command line arguments
 import random  # to generate random preferences
 
+
+
 class Person():
     '''
     A person class, defined by their prefernce list, boolean if they are male, 
@@ -14,22 +16,26 @@ class Person():
         self.name = name  # string
         self.male = male  # boolean, not really used
         self.fiance = None
+        self.ugly = None  # the person this person finds the least attractive
 
     def generateRandPref(self, listOfOtherGender):
         self.pref = list(listOfOtherGender)
         random.shuffle(self.pref)  # does inline, returns nothing!
-    
+        self.ugly = self.pref[len(self.pref)-1]  # get the least attractive person
+        print("     TEST: " + self.name + "DOESN'T LIKE: " + self.ugly.name + "\n\n")
+
     def getPref(self, spacing=8):
         """Utility function, Prints out preference list"""
         for person in self.pref:
             print(person.name.ljust(spacing), end=" ")  # no newline
 
-
 def main(numberOfCouples, verbose): 
     num = numberOfCouples
     men = []
     women = []
-    proposals = 0
+    proposals = 0  # when man approaches woman
+    rejections = 0  # when man is rejected upton approach
+    breakups = 0  # when woman breaks up the engagement, to be with a "better man"
     for i in range(0,num):
         men.append(Person("man" + str(i), True))
         women.append(Person("woman" + str(i), False))
@@ -58,7 +64,6 @@ def main(numberOfCouples, verbose):
         proposals += 1
         readyMan = lonelyMen.pop(0)  # get man at top of "stack"
         if(verbose): print(readyMan.name + " is looking for a fiance...") 
-
         crush = readyMan.pref.pop(0)  # pop top woman in "stack"
         if(verbose): print(" " + readyMan.name + " is proposing to: " + crush.name)
         # and see if woman is engaged
@@ -70,12 +75,12 @@ def main(numberOfCouples, verbose):
         else: # w is currently engaged with someone else
             otherMan = crush.fiance
             if (crush.pref.index(readyMan) > crush.pref.index(otherMan)):
-                if(verbose): 
-                    if(verbose): print(" " + readyMan.name + " got dejected by " + crush.name)
+                if(verbose): print(" " + readyMan.name + " got rejected by " + crush.name)
                 lonelyMen.append(readyMan)  # no longer ready, push to top of stack
+                rejections += 1
             else:  # our readyMan is liked BETTER
-                if(verbose): 
-                    if(verbose): print("  " + readyMan.name + " is liked better than " + otherMan.name + " " + otherMan.name + " ")
+                if(verbose): print("  " + readyMan.name + " is liked better than " + otherMan.name + " ")
+                breakups += 1
                 otherMan.fiance = None
                 lonelyMen.append(otherMan)  # no longer ready, add back to list
                 crush.fiance = readyMan
@@ -83,13 +88,23 @@ def main(numberOfCouples, verbose):
         if(verbose): print("")
     if(verbose): print("==========End Engagements=========")
     for man in men:
-        print("Couple: " + man.name + " <---> " + man.fiance.name)
+        print(("Couple: " + man.name + " <---> " + man.fiance.name), end="") 
+        if(man.ugly is man.fiance):
+            print(" | " + man.name + " got their least desirable match! " + man.ugly.name ,end="")
+        print("")
     print("\n------------------------")
-    print("Algorithm Stats:")
-    print(" n = " + str(numberOfCouples))
+    print("Couples(n) = " + str(numberOfCouples))
+    print("Total proposals: " + str(proposals))
+    print("Rejections: " + str(rejections))
+    print("Breakups: " + str(breakups))
+    print("")
+    print("Big O:")
     n2 = numberOfCouples * numberOfCouples
     print(" n^2 = " + str(n2))
-    print("Proposals: " + str(proposals))
+    print("Full Algorithm analysis:")
+    print("  n^2-n+1 where n = " + str(numberOfCouples))
+    fullAlgo = (numberOfCouples * numberOfCouples)-numberOfCouples+1
+    print("  Algorithm gives max worstcase:(" + str(fullAlgo) + ")")
 
 if __name__ == '__main__':
     if len(sys.argv) == 1: 
